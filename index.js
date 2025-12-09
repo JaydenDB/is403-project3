4363534f2751b0b0eb3c793b14820f2ed2718b5b
JaydenDB <35471045+JaydenDB@users.noreply.github.com>
2025-12-09 13:31:07 -0700

    database connection

--- index.js
+++ index.js
@@ -23,22 +23,19 @@ app.use(
     })
 );
 
-// ==========================
-// ✅ KNEX CONNECTION
-// ==========================
 const knex = require("knex")({
     client: "pg",
     connection: {
       host: process.env.DB_HOST || "project3database.cy708im2qmf7.us-east-1.rds.amazonaws.com",
       user: process.env.DB_USER || "postgres",
       password: process.env.DB_PASSWORD || "project3password",
-      database: process.env.DB_NAME || "project3database",
+      database: process.env.DB_NAME || "postgres",
       port: Number(process.env.DB_PORT) || 5432,
       ssl: process.env.DB_SSL === "false"
         ? false
         : { rejectUnauthorized: false }
     }
-  });  
+});
 
 // ==========================
 // ✅ SESSION AVAILABLE TO EJS
